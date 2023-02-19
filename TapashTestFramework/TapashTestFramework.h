df7bd3be978024f55b9f26cf55488cb2a33efebd
Tapash Mollick <tapashmollick@mplgaming.com>
2023-02-19 17:05:16 +0530

    first commit

---
+++ TapashTestFramework/TapashTestFramework.h
@@ -0,0 +1,18 @@
+//
+//  TapashTestFramework.h
+//  TapashTestFramework
+//
+//  Created by Tapash Mollick on 19/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TapashTestFramework.
+FOUNDATION_EXPORT double TapashTestFrameworkVersionNumber;
+
+//! Project version string for TapashTestFramework.
+FOUNDATION_EXPORT const unsigned char TapashTestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapashTestFramework/PublicHeader.h>
+
+
